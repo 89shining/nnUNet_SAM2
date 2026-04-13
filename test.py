ad8eb97e9c27d20cf57b6350ff596ff2a44d45df
89shining <wusignature@163.com>
2026-04-13 15:51:24 +0800

    nnUNet Baseline

---
+++ test.py
@@ -0,0 +1,5 @@
+set nnUNet_preprocessed=D:/learning/nnUNet/nnUNetFrame/nnUNet_preprocessed
+set nnUNet_raw=D:/learning/nnUNet/nnUNetFrame/nnUNet_raw
+set nnUNet_results=D:/learning/nnUNet/nnUNetFrame/nnUNet_results
+
+nnUNetv2_plan_and_preprocess -d 001 --verify_dataset_integrity
